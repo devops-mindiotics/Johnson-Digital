d815a0bfe2973ea4b6d10faa60124f4d7c3bc855
Narayana Papolu <Papolu.Narayana1@gmail.com>
2025-09-04 18:51:06 +0000

    No, Stillmoving

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -39,7 +39,7 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
           <SidebarNav />
         </Sidebar>
         <SidebarInset>
-          <div className="flex flex-col h-screen">
+          <div className="flex flex-col h-screen overflow-hidden">
             <DashboardHeader />
             <main className="flex-1 overflow-y-auto p-4 sm:p-6 lg:p-8">{children}</main>
             <DashboardFooter />
