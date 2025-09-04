888984612644455134515b00780b0bc85d7c6239
Narayana Papolu <Papolu.Narayana1@gmail.com>
2025-09-04 19:10:14 +0000

    My class still getting error

--- src/app/dashboard/my-classes/page.tsx
+++ src/app/dashboard/my-classes/page.tsx
@@ -1,5 +1,5 @@
 
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle, CardFooter } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import {
   Table,
