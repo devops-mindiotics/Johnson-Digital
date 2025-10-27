752af39465891007a093d0933312ccf86f0e11b1
Saritha <saritha@mindiotics.com>
2025-10-27 11:27:29 +0530

    School api integration

--- src/lib/api/schoolApi.ts
+++ src/lib/api/schoolApi.ts
@@ -1,28 +1,33 @@
-import apiClient from './client';
-import { setTokens,setJWT } from '@/lib/utils/token';
-import type { SchoolCreateResponse } from '@/types/school/schoolCreateResponse';
-import { API_BASE_URL } from '@/lib/utils/constants';
-import { saveRoles } from '@/lib/utils/getRole';
+import apiClient from "./client";
+import { setTokens, setJWT } from "@/lib/utils/token";
+import type { SchoolCreateResponse } from "@/types/school/schoolCreateResponse";
+import { API_BASE_URL } from "@/lib/utils/constants";
+import { saveRoles } from "@/lib/utils/getRole";
+import axios from "axios";
 
+export async function createSchool(
+  tenantId: string,
+  schoolPayload: any
+): Promise<SchoolCreateResponse> {
+  try {
+    const token = localStorage.getItem("sessionJWT");
 
-export async function createSchool(): Promise<SchoolCreateResponse> {
+    if (!token) throw new Error("User not authenticated");
 
-  const phone = mobile;
-  console.log('🌍 API Base URL loginUser:', API_BASE_URL);
+    const response = await axios.post(
+      `${API_BASE_URL}/tenants/${tenantId}/schools`,
+      schoolPayload,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
-  console.log("🚀 Unable to login. formattedMobile", { phone });
-  const response = await apiClient.post('/auth/login', { phone, password });
-  const data = response.data;
-
-  if(data != null){
-    setJWT(data);
-    saveRoles(data);
+    return response.data;
+  } catch (err: any) {
+    console.error("❌ createSchool error:", err.response?.data || err.message);
+    throw err;
   }
-  
-  if (data?.data?.tokens) {
-    setTokens(data.data.tokens.accessToken, data.data.tokens.refreshToken);
-    
-  }
-
-  return data;
 }
